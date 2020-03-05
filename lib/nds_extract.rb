41aa35054adbadbf66112633f8364dbcb985441b
Alexander Silver <asilver7493@gmail.com>
2020-03-05 22:39:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,7 @@ require "pry"
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  binding.pry
+  #binding.pry
   directors_totals(director_data)
    
 end
